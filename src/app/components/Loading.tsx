38483c417c69ce3deeea73dc21530a87b48a6b9f
Berthet <lu.berthet@gmail.com>
2025-02-01 20:44:56 +0100

    justify something to my own self

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -4,6 +4,6 @@ import { orbit } from 'ldrs';
 export const Loading = () => {
   orbit.register();
 
-  // @ts-expect-error
+  // @ts-expect-error - works but types are not defined correctly have to correct it.
   return <l-orbit color="white" size="35" speed="1.5"></l-orbit>;
 };
