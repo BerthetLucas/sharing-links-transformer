d32a8b288b962624260f81425d9b502f5284a477
Berthet <lu.berthet@gmail.com>
2025-02-20 10:30:02 +0100

    add responsive behavior

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -29,13 +29,13 @@ export default function Error({ reset }: ErrorProps) {
         <p className="text-xl font-bold">Oooops something went wrong</p>
       </div>
 
-      <div className="flex w-1/2 flex-col items-start gap-2">
+      <div className="flex w-1/2 flex-col items-center gap-2 md:items-start">
         <div className="flex items-center gap-2">
           <InfoIcon size="20" />
           <p className="font-bold">Information</p>
         </div>
 
-        <div className="flex flex-col text-justify">
+        <div className="flex flex-col text-center md:text-justify">
           <p>
             It can happen that the libraries between streaming services are not the same, so either the song you are
             looking for is not available, or your link is broken. Another possibility is that I did a poor job, in which
