4254f3fbdf25d9a0a808f575a1ee72c877d2de6b
Berthet <lu.berthet@gmail.com>
2025-02-20 15:15:06 +0100

    fix wrong dependancy use

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -1,9 +1,3 @@
-import 'ldrs/orbit';
-import { orbit } from 'ldrs';
-
 export const Loading = () => {
-  orbit.register();
-
-  // @ts-expect-error - works but types are not defined correctly have to correct it.
-  return <l-orbit color="white" size="35" speed="1.5"></l-orbit>;
+  return <p>Loading...</p>;
 };
