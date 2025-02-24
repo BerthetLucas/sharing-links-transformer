f6c8fb0a2ab8be26cfd7e67c1b6187c663b609d1
Berthet <lu.berthet@gmail.com>
2025-02-25 00:00:14 +0100

    change url

--- e2e/share-spotify-link.spec.ts
+++ e2e/share-spotify-link.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('When I share a valid Spotify link, I should get a Deezer Link', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('http://localhost:3000');
   await expect(page.getByText('From Spotify to Deezer and the other way around')).toBeVisible();
   const inputUrl = page.getByTestId('form-url-input');
   await inputUrl.fill('https://open.spotify.com/intl-fr/track/4CeeEOM32jQcH3eN9Q2dGj?si=44049a0115ee41d2');
