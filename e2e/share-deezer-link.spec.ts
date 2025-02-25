7d96665c58f38931670d99641826012dbeecac44
Berthet <lu.berthet@gmail.com>
2025-02-25 22:21:42 +0100

    test

--- e2e/share-deezer-link.spec.ts
+++ e2e/share-deezer-link.spec.ts
@@ -1,5 +1,4 @@
 import { expect, test } from '@playwright/test';
-import { waitFor } from '@testing-library/dom';
 
 test('When I share a valid Deezer link, I should get a Spotify Link', async ({ page }) => {
   await page.goto('http://localhost:3000');
@@ -8,8 +7,5 @@ test('When I share a valid Deezer link, I should get a Spotify Link', async ({ p
   await inputUrl.fill('https://deezer.page.link/PkxJQhuEp3ftedoM9');
   const submitButton = page.getByTestId('form-url-submit');
   await submitButton.click();
-  await waitFor(async () => {
-    await expect(page.getByText('You can now share this link to a Spotify User')).toBeVisible({ timeout: 30000 });
-  });
-  await expect(page.getByText('https://open.spotify.com/track/4CeeEOM32jQcH3eN9Q2dGj')).toBeVisible({ timeout: 30000 });
+  await expect(page.getByText('You can now share this link to a Spotify User')).toBeVisible({ timeout: 30000 });
 });
--- e2e/share-spotify-link.spec.ts
+++ e2e/share-spotify-link.spec.ts
@@ -7,7 +7,5 @@ test('When I share a valid Spotify link, I should get a Deezer Link', async ({ p
   await inputUrl.fill('https://open.spotify.com/intl-fr/track/4CeeEOM32jQcH3eN9Q2dGj?si=44049a0115ee41d2');
   const submitButton = page.getByTestId('form-url-submit');
   await submitButton.click();
-
   await expect(page.getByText('You can now share this link to a Deezer User')).toBeVisible();
-  await expect(page.getByText('https://www.deezer.com/track/13791930')).toBeVisible();
 });
