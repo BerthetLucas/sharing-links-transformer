6ddf1f4d5253451ad8738c3247c4b44d4e783a9b
Berthet <lu.berthet@gmail.com>
2025-02-27 15:06:39 +0100

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,37 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'next-template',
-  description: 'A simple Next.js starter template',
+  title: 'Convert Streaming Music Link Platform',
+  description: 'A website to change Spotify sharing links into Deezer ones',
+  keywords: ['Spotify', 'Deezer', 'Music', 'Link Converter', 'Sharing'],
+  authors: [{ name: 'BerthetLucas' }],
+  creator: 'BerthetLucas',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://sharing-links-transformer.vercel.app/',
+    title: 'Convert Streaming Music Link Platform',
+    description: 'A website to change Spotify sharing links into Deezer ones',
+    images: [
+      {
+        url: '/website.png',
+        width: 1200,
+        height: 630,
+        alt: 'Sharing Link Platform',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Convert Streaming Music Link Platform',
+    description: 'A website to change Spotify sharing links into Deezer ones',
+    images: ['https://www.yourwebsite.com/og-image.png'],
+  },
+  icons: {
+    icon: '/audio-lines.png',
+    shortcut: '/audio-lines.png',
+    apple: '/audio-lines.png',
+  },
 };
 
 export default function RootLayout({
