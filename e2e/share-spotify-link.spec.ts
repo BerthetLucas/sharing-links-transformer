59d40afc68594e6d1ce19efc8ce57951c32376a7
Berthet <lu.berthet@gmail.com>
2025-02-27 14:48:19 +0100

    add error page test

--- e2e/share-spotify-link.spec.ts
+++ e2e/share-spotify-link.spec.ts
@@ -7,6 +7,10 @@ test('When I share a valid Spotify link, I should get a Deezer Link', async ({ p
   await inputUrl.fill('https://open.spotify.com/intl-fr/track/4CeeEOM32jQcH3eN9Q2dGj?si=44049a0115ee41d2');
   const submitButton = page.getByTestId('form-url-submit');
   await submitButton.click();
-  await expect(page.getByText('You can now share this link to a Deezer User')).toBeVisible({ timeout: 20000 });
-  await expect(page.getByText('https://www.deezer.com/track/13791930')).toBeVisible({ timeout: 20000 });
+  await expect(page.getByText('You can now share this link to a Deezer User')).toBeVisible();
+  await expect(page.getByText('Nirvana')).toBeVisible();
+  await expect(page.getByText('Smells Like Teen Spirit')).toBeVisible();
+  const retryButton = page.getByTestId('retry-button');
+  await retryButton.click();
+  await expect(page.getByText('From Spotify to Deezer and the other way around')).toBeVisible();
 });
