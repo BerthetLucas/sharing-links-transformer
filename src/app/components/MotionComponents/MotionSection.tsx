de6eac69a3dd0625c92074b9c1ee196fdc507778
Berthet <lu.berthet@gmail.com>
2025-02-28 17:40:58 +0100

    fix prettier

--- src/app/components/MotionComponents/MotionSection.tsx
+++ src/app/components/MotionComponents/MotionSection.tsx
@@ -1,23 +1,22 @@
-import {motion} from "framer-motion";
-import type {ReactNode} from "react";
-
+import { motion } from 'framer-motion';
+import type { ReactNode } from 'react';
 
 type MotionSectionProps = {
   children: ReactNode;
   className?: string;
-  delay?: number
-}
+  delay?: number;
+};
 
-export const MotionSection = ({children, className, delay}: MotionSectionProps) => {
+export const MotionSection = ({ children, className, delay }: MotionSectionProps) => {
   return (
     <motion.section
       className={className}
-      initial={{opacity: 0}}
-      transition={{duration: 1, delay: delay ?? 0}}
-      viewport={{once: true}}
-      whileInView={{opacity: 1}}
+      initial={{ opacity: 0 }}
+      transition={{ duration: 1, delay: delay ?? 0 }}
+      viewport={{ once: true }}
+      whileInView={{ opacity: 1 }}
     >
       {children}
     </motion.section>
-  )
-}
+  );
+};
