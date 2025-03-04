4d958785226767e57e8325d55c360f135db1b2b0
BerthetLucas <lu.berthet@gmail.com>
2025-03-04 07:22:48 +0100

    first refacto

---
+++ src/app/hooks/useLinkTransformer.ts
@@ -0,0 +1,42 @@
+import { useAtom } from 'jotai';
+import { deezerSongIdAtom, deezerSongUrlAtom, inputUrlAtom, spotifySongIdAtom, useReset } from '@/app/store/linksAtoms';
+import type { FormEvent } from 'react';
+
+export const useLinkTransformer = () => {
+  const { resetDeezerSongUrl, resetSpotifySongId, resetDeezerSongId } = useReset();
+  const [inputUrl, setInputUrl] = useAtom(inputUrlAtom);
+  const [spotifySongId, setSpotifySongId] = useAtom(spotifySongIdAtom);
+  const [deezerSongUrl, setDeezerSongUrl] = useAtom(deezerSongUrlAtom);
+  const [deezerSongId, setDeezerSongId] = useAtom(deezerSongIdAtom);
+
+  const handleSubmit = (event: FormEvent) => {
+    resetDeezerSongUrl();
+    resetSpotifySongId();
+    resetDeezerSongId();
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const url = formData.get('url') as string;
+    setInputUrl(url);
+  };
+
+  const splitedURL = inputUrl.split('/');
+  const songId = splitedURL.at(-1);
+
+  if (songId && splitedURL[2] === 'open.spotify.com') {
+    setSpotifySongId(songId);
+  }
+  if (songId && splitedURL[2] === 'deezer.page.link') {
+    setDeezerSongUrl(inputUrl);
+  }
+
+  if (songId && splitedURL[2] === 'www.deezer.com') {
+    setDeezerSongId(songId);
+  }
+  
+  return {
+    spotifySongId,
+    deezerSongId,
+    deezerSongUrl,
+    handleSubmit,
+  };
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,14 @@
 'use client';
-import { useAtom } from 'jotai';
-import { RESET } from 'jotai/utils';
-import { Suspense, useEffect } from 'react';
+import { Suspense } from 'react';
 import { DeezerLink } from '@/app/components/DeezerLink';
 import { FormInputLink } from '@/app/components/FormInputLink';
 import { Loading } from '@/app/components/Loading';
 import { SpotifyLink } from '@/app/components/SpotifyLink/SpotifyLink';
 import { Title } from '@/app/components/Title';
-import { deezerSongIdAtom, deezerSongUrlAtom, inputUrlAtom, spotifySongIdAtom } from '@/app/store/linksAtoms';
-import type { FormEvent } from 'react';
+import { useLinkTransformer } from '@/app/hooks/useLinkTransformer';
 
 export default function HomePage() {
-  const [inputUrl, setInputUrl] = useAtom(inputUrlAtom);
-  const [spotifySongId, setSpotifySongId] = useAtom(spotifySongIdAtom);
-  const [deezerSongUrl, setDeezerSongUrl] = useAtom(deezerSongUrlAtom);
-  const [deezerSongId, setDeezerSongId] = useAtom(deezerSongIdAtom);
-
-  function handleSubmit(event: FormEvent) {
-    setDeezerSongUrl(RESET);
-    setSpotifySongId(RESET);
-    setDeezerSongId(RESET);
-    event.preventDefault();
-    const formData = new FormData(event.target as HTMLFormElement);
-    const url = formData.get('url') as string;
-    setInputUrl(url);
-  }
-
-  useEffect(() => {
-    if (!inputUrl) {
-      return;
-    }
-    const splitedURL = inputUrl.split('/');
-    const songId = splitedURL.at(-1);
-
-    if (songId && splitedURL[2] === 'open.spotify.com') {
-      setSpotifySongId(songId);
-    }
-    if (songId && splitedURL[2] === 'deezer.page.link') {
-      setDeezerSongUrl(inputUrl);
-    }
-
-    if (songId && splitedURL[2] === 'www.deezer.com') {
-      setDeezerSongId(songId);
-    }
-  }, [inputUrl, setDeezerSongId, setDeezerSongUrl, setSpotifySongId]);
+  const { deezerSongId, spotifySongId, deezerSongUrl, handleSubmit } = useLinkTransformer();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-8 md:p-24">
