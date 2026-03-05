20def36273c9447fc74c96629c073a056e7c6861
Lucas Berthet <lu.berthet@gmail.com>
2026-03-05 23:34:48 +0100

    fix CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,10 +19,8 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 2 : undefined,
 
   /* Reporter to use. */
   reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
