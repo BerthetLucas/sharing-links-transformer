91230b8832a0e18b386814b2160eb26a544c1c17
Lucas Berthet <lu.berthet@gmail.com>
2026-03-10 20:43:42 +0100

    fix test

--- e2e/share-spotify-link.spec.ts
+++ e2e/share-spotify-link.spec.ts
@@ -4,12 +4,12 @@ test('When I share a valid Spotify link, I should get a Deezer Link', async ({ p
   await page.goto('http://localhost:3000');
   await expect(page.getByText('From Spotify to Deezer and the other way around')).toBeVisible();
   const inputUrl = page.getByTestId('form-url-input');
-  await inputUrl.fill('https://open.spotify.com/intl-fr/track/4CeeEOM32jQcH3eN9Q2dGj?si=44049a0115ee41d2');
+  await inputUrl.fill('https://open.spotify.com/intl-fr/track/2RsAajgo0g7bMCHxwH3Sk0?si=fc766682c0094098');
   const submitButton = page.getByTestId('form-url-submit');
   await submitButton.click();
-  await expect(page.getByText('You can now share this link to a Deezer User')).toBeVisible({ timeout: 20000 });
+  await expect(page.getByText('You can now share this link to a Deezer User')).toBeVisible({ timeout: 15000 });
   await expect(page.getByText('Nirvana')).toBeVisible();
-  await expect(page.getByText('Smells Like Teen Spirit')).toBeVisible();
+  await expect(page.getByText('Come As You Are')).toBeVisible();
   const retryButton = page.getByTestId('retry-button');
   await retryButton.click();
   await expect(page.getByText('From Spotify to Deezer and the other way around')).toBeVisible();
