7c1813d1722e6ade2799ba89c23b69aa5d7f36cf
Lucas Berthet <lu.berthet@gmail.com>
2026-08-03 21:33:10 +0200

    refactor: collapse linksAtoms to a single inputUrlAtom

--- src/app/store/linksAtoms.ts
+++ src/app/store/linksAtoms.ts
@@ -1,28 +1,12 @@
 import { atomWithReset, useResetAtom } from 'jotai/utils';
 
 export const inputUrlAtom = atomWithReset('');
-export const spotifySongIdAtom = atomWithReset('');
-export const deezerSongUrlAtom = atomWithReset('');
-export const deezerSongIdAtom = atomWithReset('');
 
 export const useReset = () => {
   const resetInputUrl = useResetAtom(inputUrlAtom);
-  const resetDeezerSongUrl = useResetAtom(deezerSongUrlAtom);
-  const resetSpotifySongId = useResetAtom(spotifySongIdAtom);
-  const resetDeezerSongId = useResetAtom(deezerSongIdAtom);
-
-  const resetAll = () => {
-    resetInputUrl();
-    resetDeezerSongUrl();
-    resetSpotifySongId();
-    resetDeezerSongId();
-  };
 
   return {
     resetInputUrl,
-    resetDeezerSongUrl,
-    resetSpotifySongId,
-    resetDeezerSongId,
-    resetAll,
+    resetAll: resetInputUrl,
   };
 };
